8209114c3b776f719e01ec0abb2f5fbbc7583766
abdur4code <abdurrahim0101001@gmail.com>
2026-07-25 04:15:01 +0530

    Started learning redux toolkit, just setup react app with vite and installed redux toolkit and created redux store

---
+++ frontend/react/practice-projects/counter-redux-yt/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <div className="flex h-screen items-center justify-center bg-black">
+      <h1 className="text-5xl font-bold text-green-500">
+        Tailwind is working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
